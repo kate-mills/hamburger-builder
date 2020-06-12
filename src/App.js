458a1b5fc89a4c17662ef8801aea8e0bba717466
Kate Mills <kate.mills1124@gmail.com>
2020-06-12 16:59:26 -0700

    Import Layout & BurgerBuilder components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
+import Layout from './components/Layout'
+import BurgerBuilder from './containers/BurgerBuilder'
 
 function App() {
   return (
     <div>
-
+      <Layout>
+        <BurgerBuilder/>
+      </Layout>
     </div>
-  );
+  ) 
 }
 
-export default App;
+export default App
