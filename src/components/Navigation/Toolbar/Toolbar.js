85fd6cc96f297ab9a2c1b2338e0edac9aeb89792
Kate Mills <kate.mills1124@gmail.com>
2020-06-18 12:08:17 -0700

    Navigation css for mobile/desktop

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import classes from './Toolbar.module.css'
 
 const Toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
-      <Logo />
-      <nav><NavigationItems/></nav>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <nav className={classes.DesktopOnly}><NavigationItems/></nav>
     </header>
   )
 }
