7cff562d3a44f93cff20fd7f990c16cb0ad497e6
Kate Mills <kate.mills1124@gmail.com>
2020-06-18 12:07:29 -0700

    Feat(SideDrawer)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import classes from './Layout.module.css'
 
 const layout = (props) => {
   return (
     <Aux>
-      <div>
-        <Toolbar /> SideDrawer, Backdrop
-      </div>
+        <Toolbar />
+        <SideDrawer />
+        Backdrop
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   )
